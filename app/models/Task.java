8fd5f8404a5b766f09ed85403182d6ca83e55dcb
Feroz Panwaskar <feroz.panwaskar@gmail.com>
2011-11-02 21:57:08 +0000

    Add a task domain object that are owned by users.

---
+++ app/models/Task.java
@@ -0,0 +1,21 @@
+package models;
+ 
+import java.util.*;
+import javax.persistence.*;
+ 
+import play.db.jpa.*;
+ 
+@Entity
+public class Task extends Model {
+    public String value;
+    public Date createdAt;
+    
+    @ManyToOne
+    public User user;
+    
+    public Task(User user, String value) {
+        this.user = user;
+        this.value = value;
+        this.createdAt = new Date();
+    }
+}
--- test/UserTest.java
+++ test/UserTest.java
@@ -39,4 +39,22 @@ public class UserTest extends UnitTest {
 	    assertNull(User.connect(email, "badpassword"));
 	    assertNull(User.connect("tom@gmail.com", password));
 	}
+	
+	@Test
+	public void createTask() {
+		User bob = User.find("byEmail", email).first();
+	    String taskValue = "My first task";
+	    new Task(bob, taskValue).save();
+	    assertEquals(1, Task.count());
+
+	    // Retrieve all posts created by Bob
+	    List<Task> bobTasks = Task.find("byUser", bob).fetch();
+		Task firstTask = bobTasks.get(0);
+
+	    assertEquals(1, bobTasks.size());
+	    assertNotNull(firstTask);
+	    assertEquals(bob, firstTask.user);
+	    assertEquals(taskValue, firstTask.value);
+	    assertNotNull(firstTask.createdAt);
+	}
 }
