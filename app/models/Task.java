83805a1f0be968a5ca1ec7255a235c4698c26651
Feroz Panwaskar <feroz.panwaskar@gmail.com>
2011-11-03 08:15:55 +0000

    Remove rest annotations for initial drop onto Heroku.

--- app/models/Task.java
+++ app/models/Task.java
@@ -5,25 +5,14 @@ import javax.persistence.*;
 import javax.xml.bind.annotation.*;
  
 import play.db.jpa.*;
-import play.modules.resteasy.crud.CRUDField;
  
 @Entity
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
-public class Task extends GenericModel {
-	@Id
-	@GeneratedValue
-	@XmlAttribute
-	public Long id;
-
-	@CRUDField(editable = true)
+public class Task extends Model {
     public String value;
 	
-	@CRUDField
     public Date createdAt;
     
     @ManyToOne
-	@XmlTransient
     public User user;
     
     public Task(User user, String value) {
